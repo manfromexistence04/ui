d5824c74ca36da5c6b06d51ea0bbd3a6e75fad67
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-10 12:20:15 +0000

    feat: new-york ui updated

--- apps/www/registry/registry.sh
+++ apps/www/registry/registry.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-source_dir="/workspace/ui/apps/www/registry/default/ui"
-dest_dir="/workspace/ui/apps/www/registry/new-york/ui"
+source_dir="/workspace/ui/apps/www/registry/default/example"
+dest_dir="/workspace/ui/apps/www/registry/new-york/example"
 
 for file in "$source_dir"/*; do
-  target_file="$dest_dir/$(basename "$file")"
-  if [ -f "$target_file" ]; then
-    target_file="$dest_dir/copy_$(basename "$file")"
+  if [ -f "$file" ]; then
+    target_file="$dest_dir/$(basename "$file")"
+    if [ -f "$target_file" ]; then
+      target_file="$dest_dir/copy_$(basename "$file")"
+    fi
+    cp "$file" "$target_file"
   fi
-  cp -r "$file" "$target_file"
 done
