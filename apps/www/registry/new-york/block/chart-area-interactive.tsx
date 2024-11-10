2cc3013ffc5f365ef659357b07039960a8afb687
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-10 11:50:26 +0000

    feat: new-york

--- apps/www/registry/new-york/block/chart-area-interactive.tsx
+++ apps/www/registry/new-york/block/chart-area-interactive.tsx
@@ -141,16 +141,15 @@ export default function Component() {
 
   const filteredData = chartData.filter((item) => {
     const date = new Date(item.date)
-    const referenceDate = new Date("2024-06-30")
+    const now = new Date()
     let daysToSubtract = 90
     if (timeRange === "30d") {
       daysToSubtract = 30
     } else if (timeRange === "7d") {
       daysToSubtract = 7
     }
-    const startDate = new Date(referenceDate)
-    startDate.setDate(startDate.getDate() - daysToSubtract)
-    return date >= startDate
+    now.setDate(now.getDate() - daysToSubtract)
+    return date >= now
   })
 
   return (
